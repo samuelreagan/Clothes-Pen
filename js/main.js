46c10fa60dcbba5278c8af4c4ac47e009e2ad5d3
clothespen <37087283+clothespen@users.noreply.github.com>
2018-03-05 15:35:38 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Open & Close Mobile Menu
+document.getElementById("openMenu").onclick = function() {
+  document.getElementById("mobile-overlay").style.width = "100vw";
+}
+
+document.getElementById("closeMenu").onclick = function() {
+  document.getElementById("mobile-overlay").style.width = "0vw";
+}
